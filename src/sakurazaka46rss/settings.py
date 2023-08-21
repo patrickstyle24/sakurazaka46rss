338e2fb259561f23b6509d5e038a872c0153b69f
kenta-hamada <08088096899a@gmail.com>
2023-08-21 13:10:48 +0900

    本番用

--- src/sakurazaka46rss/settings.py
+++ src/sakurazaka46rss/settings.py
@@ -15,7 +15,7 @@ import os
 from django.conf.global_settings import DATETIME_INPUT_FORMATS, DATE_INPUT_FORMATS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
